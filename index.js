185887d3b7eae9407a1afae371dafafe3a63312f
Georgia Harby <georgiaharby@Georgias-MacBook.local>
2017-03-09 09:51:54 +0000

    undid rewrite.

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 $(function () {
+  var controller = {
+    catImage: function (slug, img) {
+      view.showCat(slug, img);
+    },
+    catClicks: function (slug) {
+      var count = 0;
+      $('.image-' + slug).click(function () {
+        count =+ 1;
+        view.cat(count);
+      });
+    }
+  };
+
+  var model = {
+    findCat: function () {
+      for (var i = 0; i < cats.catsList.length; i++) {
+        cat = cats.catsList[i];
+        controller.catImage(cat.slug, cat.img);
+      }
+    },
+    findImage: function () {
+      for (var x = 0; x < cats.catsList.length; x++) {
+        cat = cats.catsList[x];
+        controller.catClicks(cat.slug);
+      }
+    }
+  };
   var view = {
     populateList: function() {
       for (var i = 0; i < cats.catsList.length; i++) {
         $('.cat_list .list').append('<li><button class="cat-' + cats.catsList[i].slug + '">' + cats.catsList[i].title + '</button></li>');
       }
     },
-    showImage: function (slug, img) {
+    showCat: function (slug, img) {
       $('.cat-' + slug).click(function () {
         $('.image').html('<img class="image-' + slug + '" src=' + img +'>');
+        $('.clicks').html('<h1>Clicks:<h2 class="count"></h2></h1>');
       });
     }
   };
-  var model = {
-    findCat: function () {
-      for (var i = 0; i < cats.catsList.length; i++) {
-        controller.init(cats.catsList[i].slug, cats.catsList[i].img);
-      }
-    }
-  };
-  var controller = {
-    init: function () {
-      view.populateList();
-      view.showImage();
-    }
-  };
-
-  controller.init();
+  // controller.catClicks();
+  view.populateList();
+  model.findCat();
 });
